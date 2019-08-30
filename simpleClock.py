7ba7631492a74c4efd72f78eee651ebe06d2d8c6
jritchey19 <52222903+jritchey19@users.noreply.github.com>
2019-08-30 15:59:18 -0500

    Add files via upload

--- simpleClock.py
+++ simpleClock.py
@@ -14,26 +14,66 @@ def main(stdscr):
     Then refreshes the seconds to increment
     by one.
     '''
-    # Make stdscr.getch non-blocking
+    # Make stdscr.getch non-blocking.
     curses.curs_set(0)
-    y,x = stdscr.getmaxyx()
+    curses.start_color()
+
+    # Need to initialize color first before it can be used.
+    # Must have a number, a foreground, and a background.
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_color(5, 1000, 0, 0)
+    curses.init_color(6, 0, 0, 0)
+    curses.init_pair(5, 5, curses.COLOR_BLACK)
+
+    y,x = stdscr.getmaxyx()  # Get the terminals screen's size.
     stdscr.nodelay(True)
-    stdscr.clear()
-    sec = int(datetime.now().second)
-    minute = int(datetime.now().minute)
-    hour = int(datetime.now().hour)
+    stdscr.clear()  # Clear the screen initially.
+
+    # Get the time from the host machine.
+    sec = datetime.now().second
+    minute = datetime.now().minute
+    hour = datetime.now().hour
+
+    # Start a loop.
     while True:
         c = stdscr.getch()
         curses.flushinp()
-        stdscr.erase() #  Deal with the flicker on the screen.
+        stdscr.erase()  # Deal with the flicker on the screen.
         # Press q to exit.
         if c == ord('q'):
             break
         # Draw Clock bars to the screen.
-        stdscr.addstr(0,0,"#" * sec)
-        stdscr.addstr(1,0,"#" * minute)
-        stdscr.addstr(2,0,"#" * hour)
-        stdscr.addstr(3,0,"=" * x)
+        stdscr.addstr(
+            0,  # Y coordinate.
+            0,  # X coordinate.
+            "#" * sec,  # String to print to screen. 
+            curses.color_pair(2)  # attribute.
+        )
+
+        # Try with custom colors.
+        stdscr.addstr(
+            1,
+            0,
+            "#" * minute, 
+            curses.color_pair(5)
+        )
+
+        # Make string the color blue and bold.
+        stdscr.addstr(
+            2,
+            0,
+            "#" * hour, 
+            curses.color_pair(4) | curses.A_BOLD  
+        )
+        stdscr.addstr(
+            3,
+            0,
+            "=" * x, 
+            curses.color_pair(1)
+        )
 
         # If digits are under 10, add a zero in front to make everything
         # 24-hour time.
@@ -50,9 +90,18 @@ def main(stdscr):
         else:
             dhour = str(hour)
 
-        stdscr.addstr(4,0, f"Time:[{dhour}:{dminute}:{dsec}]")
-        stdscr.addstr(5,0,"Press q to quit.")
-
+        stdscr.addstr(
+            4,
+            0, 
+            f"Time:[{dhour}:{dminute}:{dsec}]", 
+            curses.color_pair(1)
+        )
+        stdscr.addstr(
+            5,
+            0,
+            "Press q to quit.", 
+            curses.color_pair(1)
+        )
         # Use time from the server.
         sec = datetime.now().second
         minute = datetime.now().minute
@@ -65,10 +114,10 @@ def main(stdscr):
             if lasMin:
                 for h in range(0, hour):
                     curses.flash()
-                    sleep(0.1) #  Wait for a brief moment.
+                    sleep(0.1)  # Wait for a brief moment.
                 lasMin = False
 
-        stdscr.refresh() #  Refresh only the parts that change on screen.
+        stdscr.refresh()  # Refresh only the parts that change on screen.
         # Wait 1/100 of a second. 
         sleep(0.01)
 
